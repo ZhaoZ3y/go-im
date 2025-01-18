01d8aebaa956832177e2b28bf65a704ef942d267
ZhaoZ3y <zzsy769174850@gmail.com>
2025-01-18 17:59:22 +0800

    ✨ 实现登录与注册，并且手搓一个jwt鉴权

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"goim/api/router"
 	"goim/config"
 	"goim/dao"
+	"goim/utils/redi"
 )
 
 func main() {
 	config.ConfigInit()
 	dao.MysqlInit()
+	redi.RedisInit()
+	router.RouterInit()
 }
